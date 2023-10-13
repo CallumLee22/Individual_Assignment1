8d84fc4b65f17d41f7e5a7ad850805078a0b4ed0
CallumLee22 <callumlee.2822@gmail.com>
2023-10-13 13:38:13 +0100

    add songs to list

--- Main.java
+++ Main.java
@@ -1,7 +1,31 @@
+import java.util.ArrayList;
+
 public class Main
 {
     public static void main(String[] args)
     {
-        
+        Song Believer = new Song("Believer", "Imagine Dragons", 1203500);
+        Song Skyfall = new Song("Skyfall", "Adele", 14527013);
+        Song Brazil = new Song("Brazil", "Declan McKenna", 123230);
+        Song Africa = new Song("Africa", "TOTO", 443629467);
+        Song Mercy = new Song("Mercy", "Shawn Mendes", 56374537);
+        Song Paris = new Song("Paris", "The Chainsmokers", 567857587);
+        Song Survival = new Song("Survival", "Eminem", 7479358);
+        Song Riptide = new Song("Riptide", "Vance Joy", 6546446);
+        Song Waterloo = new Song("Waterloo", "ABBA", 864946678);
+        Song Perfect = new Song("Perfect", "Ed Sheeran", 664676466);
+
+        ArrayList<Song> songList = new ArrayList<Song>();
+
+        songList.add(Believer);
+        songList.add(Skyfall);
+        songList.add(Brazil);
+        songList.add(Africa);
+        songList.add(Mercy);
+        songList.add(Paris);
+        songList.add(Survival);
+        songList.add(Riptide);
+        songList.add(Waterloo);
+        songList.add(Perfect);
     }
 }
