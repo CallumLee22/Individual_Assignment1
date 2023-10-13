bfdeb46477b543823719d1855a687c76effd540b
CallumLee22 <callumlee.2822@gmail.com>
2023-10-13 13:04:21 +0100

    classes made

---
+++ Song.java
@@ -0,0 +1,13 @@
+public class Song
+{
+    String title;
+    String artist;
+    int playCount;
+
+    public Song(String title, String artist, int playCount)
+    {
+        this.title = title;
+        this.artist = artist;
+        this.playCount = playCount; 
+    }
+}
